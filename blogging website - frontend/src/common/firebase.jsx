3796c5d74be00f4a17eee8a80fe6ea9282533bc4
Adolfo Alcaraz <adolgdl@gmail.com>
2024-04-12 09:22:50 -0600

    correction with api secret

--- blogging website - frontend/src/common/firebase.jsx
+++ blogging website - frontend/src/common/firebase.jsx
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCTRuGgvs_7agVjYZ9l6cMq3kOpUxZ9cH4",
-  authDomain: "react-js-blog-website-yt.firebaseapp.com",
-  projectId: "react-js-blog-website-yt",
-  storageBucket: "react-js-blog-website-yt.appspot.com",
-  messagingSenderId: "511273583112",
-  appId: "1:511273583112:web:8f691fcae60cbc4aa807bd"
+
 };
 
 const app = initializeApp(firebaseConfig);
